e45ab76c1ba5187b609f447e3b2608cf2630c0a7
saiyad akil <saiyadakil44@gmail.com>
2026-06-06 07:03:06 +0530

    Add React Header component

---
+++ Day-08-React-Examples/src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="header">
+      <h1>React Examples</h1>
+      <p>Component, props, state and list rendering practice</p>
+    </header>
+  );
+}
+
+export default Header;
